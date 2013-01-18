af8389370823e82c4d3b3848c87aa15e52cdbac9
Jan Funke <funke@ini.ch>
2013-01-18 11:43:50 +0100

    added ImageStackDirectoryWriter

---
+++ io/ImageStackDirectoryWriter.cpp
@@ -0,0 +1,71 @@
+#include <boost/lexical_cast.hpp>
+#include <vigra/impex.hxx>
+
+#include <util/Logger.h>
+
+#include "ImageStackDirectoryWriter.h"
+
+logger::LogChannel imagestackdirectorywriterlog("imagestackdirectorywriterlog", "[ImageStackDirectoryWriter] ");
+
+ImageStackDirectoryWriter::ImageStackDirectoryWriter(std::string directory, std::string basename) :
+	_directory(directory),
+	_basename(basename),
+	_dirty(false) {
+
+	registerInput(_stack, "image stack");
+
+	_stack.registerBackwardCallback(&ImageStackDirectoryWriter::onModified, this);
+}
+
+void
+ImageStackDirectoryWriter::onModified(const pipeline::Modified&) {
+
+	boost::mutex::scoped_lock lock(_dirtyMutex);
+
+	LOG_DEBUG(imagestackdirectorywriterlog) << "stack changed" << std::endl;
+
+	_dirty = true;
+}
+
+bool
+ImageStackDirectoryWriter::write(std::string basename) {
+
+	{
+		boost::mutex::scoped_lock lock(_dirtyMutex);
+
+		if (!_dirty)
+			return false;
+
+		_dirty = false;
+	}
+
+	LOG_DEBUG(imagestackdirectorywriterlog) << "attempting to write stack" << std::endl;
+
+	if (!_stack) {
+
+		LOG_ERROR(imagestackdirectorywriterlog) << "no input image stack set" << std::endl;
+		return false;
+	}
+
+	LOG_DEBUG(imagestackdirectorywriterlog) << "requesting image update" << std::endl;
+
+	updateInputs();
+
+	boost::unique_lock<boost::shared_mutex> lock(_stack->getMutex());
+
+	// save to file
+
+	unsigned int i = 0;
+
+	foreach (boost::shared_ptr<Image> image, *_stack) {
+
+		std::string filename = _directory + "/" + _basename + basename + boost::lexical_cast<std::string>(i) + ".png";
+		vigra::exportImage(vigra::srcImageRange(*image), vigra::ImageExportInfo(filename.c_str()));
+
+		i++;
+	}
+
+	LOG_DEBUG(imagestackdirectorywriterlog) << "images written" << std::endl;
+
+	return true;
+}
---
+++ io/ImageStackDirectoryWriter.h
@@ -0,0 +1,33 @@
+#ifndef IMAGEPROCESSING_IO_IMAGE_STACK_DIRECTORY_WRITER_H__
+#define IMAGEPROCESSING_IO_IMAGE_STACK_DIRECTORY_WRITER_H__
+
+#include <string>
+
+#include <pipeline/all.h>
+#include <imageprocessing/ImageStack.h>
+
+class ImageStackDirectoryWriter : public pipeline::SimpleProcessNode<> {
+
+public:
+
+	ImageStackDirectoryWriter(std::string directory, std::string basename);
+
+	bool write(std::string basename = "");
+
+private:
+
+	void updateOutputs() {}
+
+	void onModified(const pipeline::Modified&);
+
+	pipeline::Input<ImageStack> _stack;
+
+	std::string _directory;
+	std::string _basename;
+
+	bool         _dirty;
+	boost::mutex _dirtyMutex;
+};
+
+#endif // IMAGEPROCESSING_IO_IMAGE_STACK_DIRECTORY_WRITER_H__
+
