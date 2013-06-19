e622ec74971797dfc6867bbfca8b52afa4aad504
Jan Funke <funke@ini.ch>
2013-06-19 18:54:31 +0200

    moved Image{View,Painter} to imageprocessing/gui

---
+++ gui/ImageView.h
@@ -0,0 +1,38 @@
+#ifndef IMAGE_VIEW_H__
+#define IMAGE_VIEW_H__
+
+#include <pipeline/all.h>
+#include <gui/MouseSignals.h>
+#include <util/Logger.h>
+#include <imageprocessing/Image.h>
+#include "ImagePainter.h"
+
+namespace gui {
+
+static logger::LogChannel imageviewlog("imageviewlog", "[ImageView] ");
+
+class ImageView : public pipeline::SimpleProcessNode<> {
+
+public:
+
+	ImageView();
+
+private:
+
+	void updateOutputs();
+
+	void onInputImageSet(const pipeline::InputSet<Image>& signal);
+
+	// input/output
+	pipeline::Input<Image>                      _image;
+	pipeline::Output<gui::ImagePainter<Image> > _painter;
+
+	// forward signals
+	signals::Slot<const gui::SizeChanged>    _sizeChanged;
+	signals::Slot<const gui::ContentChanged> _contentChanged;
+};
+
+} // namespace gui
+
+#endif // IMAGE_VIEW_H__
+
