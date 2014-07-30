ca4c321c16327b0be09b3a494477e020a7092d2c
Jan Funke <funke@ini.ch>
2014-07-30 10:13:32 +0200

    changes ImagePainter to get colors from gui/Colors.h

--- gui/ImagePainter.h
+++ gui/ImagePainter.h
@@ -4,6 +4,7 @@
 #include <boost/concept_check.hpp>
 #include <boost/lexical_cast.hpp>
 
+#include <gui/Colors.h>
 #include <gui/ContextSettings.h>
 #include <gui/GlContext.h>
 #include <gui/OpenGl.h>
@@ -139,11 +140,6 @@ private:
 	 * Re-load image for multi-channel images.
 	 */
 	void load(const boost::false_type& arithmetic);
-
-	/**
-	 * Get the RGB values for hsv.
-	 */
-	void hsvToRgb(double h, double s, double v, unsigned char& r, unsigned char& g, unsigned char& b);
 
 	/**
 	 * Entry point for the texture reload thread.
@@ -544,10 +540,7 @@ ImagePainter<Image, Pointer>::load(const boost::true_type&) {
 			// intensities above one are handled as color indices
 			if (*i >= 1.0) {
 
-				float h = fmod(static_cast<float>(*i)*M_PI, 1.0);
-				float s = 0.5 + fmod(static_cast<float>(*i)*M_PI*2, 0.5);
-				float v = (*i == 0 ? 0.0 : 0.75 + fmod(static_cast<float>(*i)*M_PI*3, 0.25));
-				hsvToRgb(h, s, v, pixel[0], pixel[1], pixel[2]);
+				idToRgb(*i, pixel[0], pixel[1], pixel[2]);
 
 			// intensities below 1 are handled as grayscale
 			} else {
@@ -574,62 +567,6 @@ ImagePainter<Image, Pointer>::load(const boost::false_type& arithmetic) {
 
 	// non-intensity images are just loaded
 	_imageTexture->loadData(&(*_image->begin()));
-}
-
-template <typename Image, typename Pointer>
-void
-ImagePainter<Image, Pointer>::hsvToRgb(double h, double s, double v, unsigned char& r, unsigned char& g, unsigned char& b) {
-
-	if(s < 0) s = 0;
-	if(s > 1) s = 1;
-	if(v < 0) v = 0;
-	if(v > 1) v = 1;
-
-	if(s == 0) {
-		r = (unsigned char)255.0*v;
-		g = (unsigned char)255.0*v;
-		b = (unsigned char)255.0*v;
-	}
-
-	h = fmod(h, 1.0); // want h to be in 0..1
-
-	unsigned int i = h*6;
-	double f = (h*6) - i;
-	double p = v*(1.0f - s); 
-	double q = v*(1.0f - s*f);
-	double t = v*(1.0f - s*(1.0f-f));
-	switch(i%6) {
-	case 0:
-		r = (unsigned char)255.0*v;
-		g = (unsigned char)255.0*t;
-		b = (unsigned char)255.0*p;
-		return;
-	case 1:
-		r = (unsigned char)255.0*q;
-		g = (unsigned char)255.0*v;
-		b = (unsigned char)255.0*p;
-		return;
-	case 2:
-		r = (unsigned char)255.0*p;
-		g = (unsigned char)255.0*v;
-		b = (unsigned char)255.0*t;
-		return;
-	case 3:
-		r = (unsigned char)255.0*p;
-		g = (unsigned char)255.0*q;
-		b = (unsigned char)255.0*v;
-		return;
-	case 4:
-		r = (unsigned char)255.0*t;
-		g = (unsigned char)255.0*p;
-		b = (unsigned char)255.0*v;
-		return;
-	case 5:
-		r = (unsigned char)255.0*v;
-		g = (unsigned char)255.0*p;
-		b = (unsigned char)255.0*q;
-		return;
-	}
 }
 
 template <typename Image, typename Pointer>
