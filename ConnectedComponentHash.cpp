d232b1d7b133ba40859abd157f5f5a6c51247e6f
Jan Funke <funke@ini.ch>
2014-10-16 13:29:33 +0200

    made ConnectedComponentHash be componted from components bitmap (not pixel list, since order is not unique)

--- ConnectedComponentHash.cpp
+++ ConnectedComponentHash.cpp
@@ -15,5 +15,18 @@ hash_value(const ConnectedComponent& component) {
 		boost::hash_combine(hash, boost::hash_value(pixel.y));
 	}
 
+	const ConnectedComponent::bitmap_type& bitmap      = component.getBitmap();
+	const util::rect<unsigned int>&        boundingBox = component.getBoundingBox();
+
+	for (int x = 0; x < bitmap.width(); ++x) {
+		for (int y = 0; y < bitmap.height(); ++y) {
+			if (bitmap(x, y)) {
+
+				boost::hash_combine(hash, boost::hash_value(x + boundingBox.minX));
+				boost::hash_combine(hash, boost::hash_value(y + boundingBox.minY));
+			}
+		}
+	}
+
 	return hash;
 }
