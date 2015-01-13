9304ebd15da1e45af76fdf46c82d9485890d29b7
Jan Funke <funke@ini.ch>
2015-01-13 18:36:35 +0100

    added convenience functions to ExplicitVolume

--- ExplicitVolume.h
+++ ExplicitVolume.h
@@ -20,6 +20,14 @@ public:
 	ExplicitVolume() {}
 
 	/**
+	 * Create a volume from another (type compatible) volume.
+	 */
+	template <typename T>
+	ExplicitVolume(const ExplicitVolume<T>& other) :
+		DiscreteVolume(other),
+		_data(other.data()) {}
+
+	/**
 	 * Create a new explicit volume of the given size.
 	 */
 	ExplicitVolume(
@@ -40,11 +48,23 @@ public:
 		_data(vigra::Shape3(width, height, depth), value) {}
 
 	/**
+	 * Pixel access.
+	 */
+	ValueType&       operator[](vigra::Shape3 pos)       { return _data[pos]; }
+	const ValueType& operator[](vigra::Shape3 pos) const { return _data[pos]; }
+	ValueType&       operator()(unsigned int x, unsigned int y, unsigned int z)       { return _data(x, y, z); }
+	const ValueType& operator()(unsigned int x, unsigned int y, unsigned int z) const { return _data(x, y, z); }
+
+	/**
 	 * Get access to the vigra multi-array that contains the data of this 
 	 * volume.
 	 */
 	data_type&       data()       { return _data; }
 	const data_type& data() const { return _data; }
+
+	unsigned int width()  const { return _data.shape()[0]; }
+	unsigned int height() const { return _data.shape()[1]; }
+	unsigned int depth()  const { return _data.shape()[2]; }
 
 private:
 
